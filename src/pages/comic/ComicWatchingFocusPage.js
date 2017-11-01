7c0cc726fe219d2830c8fbbeba6f376ffb40dc36
liuxipeng <1606020697@qq.com>
2017-11-01 15:26:28 +0800

    ComicRN项目的基本代码提交

--- src/pages/comic/ComicWatchingFocusPage.js
+++ src/pages/comic/ComicWatchingFocusPage.js
@@ -13,6 +13,7 @@ export default class ComicWatchingFocusPage extends PureComponent {
             <View 
                 style = {{
                     flexDirection: 'row',
+
                     justifyContent: 'flex-start',
                 }}
             >
