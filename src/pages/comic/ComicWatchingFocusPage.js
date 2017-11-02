c58288d9127ce2eab21cf875dc86ddf08d449408
liuxipeng <1606020697@qq.com>
2017-11-02 10:21:42 +0800

    解决漫画精彩看点详情页图片展示

--- src/pages/comic/ComicWatchingFocusPage.js
+++ src/pages/comic/ComicWatchingFocusPage.js
@@ -1,96 +1,76 @@
 //漫画精彩看点详情页
 import React, { PureComponent } from 'react'
 import {StyleSheet, Image, Text, View, ScrollView,TouchableWithoutFeedback} from 'react-native'
+import ViewPager from 'react-native-viewpager';
+import screen from '../../common/screen'
+
 export default class ComicWatchingFocusPage extends PureComponent {
 
-    render() {
-        let item = this.props.item;
-        var pos=this.props.navigation.state.params.pos
+    constructor(props) {
+        super(props);
         var imgs=this.props.navigation.state.params.imgs
-        console.log(imgs[0])
+        // 用于构建DataSource对象
+        var dataSource = new ViewPager.DataSource({
+            pageHasChanged: (p1, p2) => p1 !== p2,
+        });
+    // 实际的DataSources存放在state中
+        this.state = {
+            dataSource: dataSource.cloneWithPages(imgs)
+        }
+    }
+    onPressImg(){
+         var thiz=this
+         thiz.props.navigation.goBack()
+    }
+    _renderPage(data,pageID) {
         return (
-        <View style = {styles.container}>
-            <View 
-                style = {{
-                    flexDirection: 'row',
-                    justifyContent: 'flex-start',
-                }}
-            >
-                <Image 
-                    source = {{uri: imgs[0]}}
-                    style = {styles.topImage}
-                >
-                </Image>
-                <Text style = {styles.title}>
-                    {pos}
-                </Text>
-            </View>
-
-
-            
-        </View>
+        <TouchableWithoutFeedback onPress={()=> this.onPressImg()}>
+            <Image
+                source={{uri:data}}
+                style={styles.page}/>
+         </TouchableWithoutFeedback>
         );
     }
-//    onPressImg(imagesData，position){
-//    }
-//    renderImages() {
-//
-//        let imagesData = this.props.item.data
-//
-//        let images = []
-//
-//        for (let i = 0; i < imagesData.length; i++) {
-//            let item = imagesData[i];
-//            images.push(
-//            <TouchableWithoutFeedback onPress={()=> this.onPressImg(imagesData,i)}>
-//                <Image key={i} source={{uri: item.img}} style={styles.image}/>
-//            </TouchableWithoutFeedback>
-//            );
-//        }
-//        return images;
-//    }
-
+     /**
+        dataSource: 提供页面数据,
+        renderPage: 用于渲染页面视图,
+        autoPlay: 为true 将自动播放,
+        isLoop: 为true支持循环播放,
+        locked: 为true禁止触摸滚动,
+        onChangePage: 页面变化的回调,
+        renderPageIndicator: 渲染自定义的 ViewPager indicator.
+        */
+    render() {
+        var pos=this.props.navigation.state.params.pos
+        return (
+                <View style={styles.container}>
+                       <ViewPager
+                         dataSource={this.state.dataSource}
+                         renderPage={this._renderPage.bind(this)}
+                         initialPage={pos}
+                         isLoop={false}
+                         autoPlay={false}/>
+                </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
 
     container: {
-        backgroundColor: 'white',
-    },
-    title: {
-        marginTop: 31,
-        marginLeft: 8,
-        width: 200,
-        height: 30,
-        backgroundColor: 'white',
-        color: 'black',
-        fontSize: 16,
-    },
-
-    topImage: {
-        width: 46,
-        height: 46,
-        marginLeft: 16,
-        marginTop: 16,
-    },
-
-    horizontalScrollView: {
-        height: 174,
-        backgroundColor: '#f3f3f3',
-    },
-    image: {
-        marginTop: 14,
-        marginLeft: 15,
-        width: 110,
-        height: 146,
-    },
-
+            flex: 1,
+            flexDirection: 'row',
+            alignItems: 'flex-start',
+            paddingTop:5,
+            paddingLeft:5,
+            backgroundColor:'#999999',
+            paddingRight:5,
+            paddingBottom:5,
+        },
+        page: {
+            width: screen.width,//设备宽(只是一种实现，此处多余)
+            flex: 1,
+            height: screen.height,
+            resizeMode: 'stretch'
+        },
 })
-//
-//            <ScrollView
-//                automaticallyAdjustContentInsets={false}
-//                horizontal={true}
-//                showsHorizontalScrollIndicator = {false}
-//                style={[styles.scrollView, styles.horizontalScrollView]}>
-//                {this.renderImages()}
-//            </ScrollView>
